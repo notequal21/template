2fdef674f563a48bf81d94f4c774a699b53b2420
notequal21 <kalorigen@gmail.com>
2021-12-09 14:38:37 +0300

    fix errors and added command npm start

--- gulpfile.js
+++ gulpfile.js
@@ -163,9 +163,9 @@ gulp.task("svgSprite", function () {
 })
 
 function fontsStyle(params) {
-    let file_content = fs.readFileSync(source_folder + '/scss/fonts.scss');
+    let file_content = fs.readFileSync(source_folder + '/scss/components/fonts.scss');
     if (file_content == '') {
-        fs.writeFile(source_folder + '/scss/fonts.scss', '', cb);
+        fs.writeFile(source_folder + '/scss/components/fonts.scss', '', cb);
         return fs.readdir(path.build.fonts, function (err, items) {
             if (items) {
                 let c_fontname;
@@ -173,7 +173,7 @@ function fontsStyle(params) {
                     let fontname = items[i].split('.');
                     fontname = fontname[0];
                     if (c_fontname != fontname) {
-                        fs.appendFile(source_folder + '/scss/fonts.scss', '@include font("' + fontname + '", "' + fontname + '", "400", "normal");\r\n', cb);
+                        fs.appendFile(source_folder + '/scss/components/fonts.scss', '@include font("' + fontname + '", "' + fontname + '", "400", "normal");\r\n', cb);
                     }
                     c_fontname = fontname;
                 }
