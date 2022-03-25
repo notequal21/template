8e5f7c6dd7ed64ee1f3a53f327077d6ba0392f35
notequal21 <kalorigen@gmail.com>
2022-03-25 21:10:00 +0300

    refactor modal js

--- #src/js/modal.js
+++ #src/js/modal.js
@@ -1,19 +1,46 @@
-/* Функция закрывает по нажатие на кнопку модальное окно */
-function bclose() {
-	$("#popup").bPopup().close();
-	return false;
-}
 
-/* Функция показывает модальное окно по нажатию на кнопку */
-function bopen() {
-	$('#popup').bPopup();
-	return false;
-}
+if (document.querySelector('.modal-open-btn')) {
+	const openBtn = document.querySelectorAll('.modal-open-btn')
+	const modal = document.querySelector('.contactus')
+	const modalBg = document.querySelector('.contactus__bg')
+	const body = document.querySelector('body')
+	const content = document.querySelectorAll('.container')
 
-/* Функция отображает модальное окно автоматически после загрузки страницы через одну секунду */
-// $( document ).ready(function() {
-// 	setTimeout(function() {
-//         /* Вызываем модальное окно */
-//         $('#popup').bPopup();	
-// 	}, 1000);
-// });
+	let toggleModal = (e) => {
+		e.preventDefault()
+
+		let div = document.createElement('div');
+		div.style.overflowY = 'scroll';
+		div.style.width = '50px';
+		div.style.height = '50px';
+		document.body.append(div);
+		let scrollWidth = div.offsetWidth - div.clientWidth;
+
+		div.remove();
+
+		if (modal.classList.contains('active')) {
+			modal.classList.remove('active')
+			body.classList.remove('lock')
+			if (window.innerWidth > 992) {
+				content.forEach((item) => {
+					item.style.maxWidth = `1320px`
+					item.style.padding = ` 0 20px`
+				})
+			}
+		} else {
+			modal.classList.add('active')
+			body.classList.add('lock')
+			if (window.innerWidth > 992) {
+				content.forEach((item) => {
+					item.style.maxWidth = `${1320 + scrollWidth}px`
+					item.style.padding = ` 0 ${scrollWidth + 20}px 0 20px`
+				})
+			}
+		}
+	}
+
+	openBtn.forEach((item) => {
+		item.addEventListener('click', toggleModal)
+	})
+	modalBg.addEventListener('click', toggleModal)
+}
